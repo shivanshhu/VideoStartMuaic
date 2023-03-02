be8bd1ebfe8b3ee04d92ce329d53aa1418b8f2fd
Snehashish Biswas <teamscenariox@gmail.com>
2023-03-02 18:57:19 +0530

    Update help.py

--- AnonX/plugins/help.py
+++ AnonX/plugins/help.py
@@ -66,7 +66,7 @@ async def helper_private(
         language = await get_lang(chat_id)
         _ = get_string(language)
         keyboard = help_pannel(_)
-        await message.reply_text(random.choice(PH_ON_P))
+        await update.reply_text(random.choice(PH_ON_P))
         await update.reply_photo(
             photo=config.START_IMG_URL,
             caption=_["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard)
