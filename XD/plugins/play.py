ff731198c621815ed006e9e75b6c2aaf4f4b7764
Champu <112340896+shivanshhu@users.noreply.github.com>
2023-03-10 09:42:43 +0530

    Update play.py

--- XD/plugins/play.py
+++ XD/plugins/play.py
@@ -3,7 +3,7 @@ import string
 from ast import ExceptHandler
 
 from pyrogram import filters
-from pyrogram.types import (InlineKeyboardMarkup, InputMediaPhoto,
+from pyrogram.types import (InlineKeyboardMarkup, InlineKeyboardButton, InputMediaPhoto,
                             Message)
 from pytgcalls.exceptions import NoActiveGroupCall
 
